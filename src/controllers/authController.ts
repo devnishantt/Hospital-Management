286a750a2888260ff63606c58a13f127d78835e4
devnishantt <nishanttgithub@gmail.com>
2026-04-04 18:47:45 +0530

    feat(auth):implement change password endpoint

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -77,6 +77,25 @@ export const refreshToken = asyncHandler(
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
-    sendSuccess(res, {accessToken:tokens.accessToken}, "Token refreshed", STATUS_CODES.OK)
+    sendSuccess(
+      res,
+      { accessToken: tokens.accessToken },
+      "Token refreshed",
+      STATUS_CODES.OK,
+    );
+  },
+);
+
+export const changePassword = asyncHandler(
+  async (req: Request, res: Response) => {
+    const token = req.headers.authorization?.split(" ")[1] || "";
+    await authService.changePassword(
+      req.user!.id,
+      req.body.currentPassword,
+      req.body.newPassword,
+      token,
+    );
+    res.clearCookie("refreshToken");
+    sendSuccess(res, null, "Password changed successfully", STATUS_CODES.OK);
   },
 );
